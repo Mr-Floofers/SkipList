ed771c9260185000e6e8c1b91be772861232f87d
Dennis Kogan <kogan.dg@gmail.com>
2018-11-04 11:49:31 -0800

    Initial Commit

---
+++ SkipList/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkipList
+{
+    public class Node<T>
+    {
+        public int level;
+        public T value;
+
+        public Node(T value, T level = 0)
+        {
+            this.value = value;
+            this.level = level;
+        }
+    }
+}
---
+++ SkipList/Skip.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkipList
+{
+    public class Skip<T> : ICollection<T>
+    {
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public Node<T> head;
+        int bigLevel = 0;
+        public void Add(T value)
+        {
+            if(head == null)
+            {
+                head.value = value;
+            }
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
